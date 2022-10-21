8094e15a30ca2dfbe60ffc8ad965ad2d0900a072
junohheo <107922900+junohheo@users.noreply.github.com>
2022-10-21 17:47:19 +0900

    Add files via upload

---
+++ todo_homework/src/main/java/codestates/todo_homework/TodoHomeworkApplication.java
@@ -0,0 +1,13 @@
+package codestates.todo_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoHomeworkApplication.class, args);
+	}
+
+}
